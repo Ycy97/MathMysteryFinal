c69c68521aab7a7fe1bbda130bb91c87d4b15c3a
Yap Cheng Yee <chengyeeyap97@gmail.com>
2025-01-17 17:02:43 +0800

    Modified renderTemplate index.html location in app.py

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    return render_template('frontend/public/index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
