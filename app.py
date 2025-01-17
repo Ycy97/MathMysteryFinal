6093fc444f2d7546794a352bdfe295f0b295631d
Yap Cheng Yee <chengyeeyap97@gmail.com>
2025-01-17 20:05:46 +0800

    Added database connection to PostgreSQL and modified signup page to add users to database

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from flask_bcrypt import Bcrypt
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL","postgresql://chengyee:HSpcoYAt98LuINAIlpwKkOAv9LPGpCiq@dpg-cu535rggph6c73dte0og-a.singapore-postgres.render.com/mathmystery")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+##User model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
 
 @app.route('/', methods=['GET'])
 def index():
@@ -13,6 +28,17 @@ def signup():
 @app.route('/login', methods=['GET'])
 def login():
     return render_template('login.html')
+
+@app.route('/signupUser', methods=['POST'])
+def gameSignup():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+    password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
+    new_user = User(username=username, pssword=password_hash)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'message': 'User registered successfully'}), 201    
 
 if __name__ == '__main__':
     app.run(debug=True)
