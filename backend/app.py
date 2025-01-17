8893be156411a390cf58a5832a8a4c32fdc309af
Yap Cheng Yee <chengyeeyap97@gmail.com>
2025-01-17 17:30:00 +0800

    modified project structure to fit Flask convention

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template, request, redirect
-
-app = Flask(__name__)
+from flask import Flask, render_template
+app = Flask(__name__, static_folder='frontemd/static')
 
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
+
+@app.route('/signup', methods=['GET'])
+def signup():
+    return render_template('signup.html')
+
+@app.route('/login', methods=['GET'])
+def login():
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
