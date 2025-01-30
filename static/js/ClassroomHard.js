4d91537e57ceb983004bdcd598291020fbbfa09c
Yap Cheng Yee <chengyeeyap97@gmail.com>
2025-01-31 00:24:09 +0800

    changed delayed call; issue with game not reading properties of items occasionally

--- static/js/ClassroomHard.js
+++ static/js/ClassroomHard.js
@@ -240,7 +240,7 @@ class ClassroomHard extends Phaser.Scene{
             } 
 
             this.canInteract = false; // Disable further interactions
-            this.time.delayedCall(100, () => { // Re-enable interactions after 500ms
+            this.time.delayedCall(500, () => { // Re-enable interactions after 500ms
                 this.canInteract = true;
             });
         
